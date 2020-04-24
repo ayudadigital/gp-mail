4914015d9985583072b17de1f3288ba43a4639d8
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-24 15:59:12 +0200

    Upgrade: Change tpbtools and TIC para Bien references with Ayuda Digital

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -2,5 +2,5 @@
 
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Generic Platform - Mail Service (c) 2020 TIC para bien"
+echo "Generic Platform - Mail Service (c) 2020 Ayuda Digital"
 echo
